5ad7ddcf40e6b54674ada00d2f83f668583d3486
yuunik <chorria.zhou@gmail.com>
2025-09-30 00:43:35 +0800

    feat: 学习至集合类型_2

--- lib/flutter_leaner/01_Dart的基础语法/05_集合类型.dart
+++ lib/flutter_leaner/01_Dart的基础语法/05_集合类型.dart
@@ -1,13 +1,12 @@
 main(List<String> args) {
   /// 1. 列表 List: [];
-  var nameList = ["abc", "cba", "nab"];
+  List nameList = ["abc", "cba", "nab", "nab"];
 
   /// 2. 集合 Set: {}
-  var movieSet = { "星际穿越", "大话西游", "盗梦空间" };
+  var movieSet = {"星际穿越", "大话西游", "盗梦空间"};
+  nameList = Set.from(nameList).toList();
+  print(nameList);
 
   /// 3. 映射 Map
-  var info = {
-    "name": "why",
-    "age": 18
-  };
+  var info = {"name": "why", "age": 18};
 }
