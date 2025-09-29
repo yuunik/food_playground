cb699d94ecb6012b75fee694b9e7de94a1d65c7b
yuunik <chorria.zhou@gmail.com>
2025-09-30 00:39:14 +0800

    feat: 学习至集合类型

---
+++ lib/flutter_leaner/01_Dart的基础语法/05_集合类型.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  /// 1. 列表 List: [];
+  var nameList = ["abc", "cba", "nab"];
+
+  /// 2. 集合 Set: {}
+  var movieSet = { "星际穿越", "大话西游", "盗梦空间" };
+
+  /// 3. 映射 Map
+  var info = {
+    "name": "why",
+    "age": 18
+  };
+}
