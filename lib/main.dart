9947abd273bf29f4230df02944fb98cc94ac0970
yuunik <chorria.zhou@gmail.com>
2025-11-09 22:20:58 +0800

    feat: 学习了compute 函数的使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:map_launcher/map_launcher.dart';
 
-/// 将需要执行动画的 Widget 放到一个 AnimatedWidget 中的 build 方法里进行返回
-/// 缺点:
-///   1.  每次都需要创建一个类
-///   2.  如果构建的 Widget 有子类, 那么子类依旧会重复地不断构建, 即不断地执行 build 方法
+// 计算总数
+int getTotal(int count) {
+  int sum = 0;
+  for (int i = 0; i < count; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -62,6 +68,14 @@ class _AppHomePageState extends State<AppHomePage>
               _offset = _curvedAnimation.value * _offset;
             });
           });
+
+    /// compute 函数可以将计算任务放到独立的 Isolate (隔离线程)中执行避免阻塞 UI 线程.
+    /// compute 要求传入的函数必须是 顶层函数（top-level function） 或 静态函数（static function），
+    /// 不能是实例方法或私有方法（即使你把它放在同一个类里）。
+    /// 这是因为 compute 会在一个新的 isolate 中运行该函数，
+    /// 而 isolate 之间不能共享对象引用（包括 this），
+    /// 也无法访问类实例的私有成员。
+    getMyIsolateSpare();
   }
 
   /// 回弹动画
@@ -81,9 +95,9 @@ class _AppHomePageState extends State<AppHomePage>
     //   coords: Coords(37.759392, -122.5107336),
     //   title: "Ocean Beach",
     // );
-    if (await MapLauncher.isMapAvailable(MapType.google)) {
+    if (await MapLauncher.isMapAvailable(MapType.amap)) {
       await MapLauncher.showDirections(
-        mapType: MapType.google,
+        mapType: MapType.amap,
         destination: Coords(24.4836, 118.1278),
         // title: "Ocean Beach",
         // description: description,
@@ -91,9 +105,15 @@ class _AppHomePageState extends State<AppHomePage>
     }
   }
 
+  // compute
+  Future getMyIsolateSpare() async {
+    final result = await compute(getTotal, 100);
+    print("总和结果为 $result");
+  }
+
   @override
   Widget build(BuildContext context) {
-    checkMap();
+    // checkMap();
     return Scaffold(
       appBar: AppBar(title: Text("Hero animation demo"), centerTitle: true),
       body: Stack(
